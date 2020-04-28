fff7e6e26bbd1d5e7ecb99004f876fa3e596294e
Muhammad Tifak <moht20009@gmail.com>
2020-04-28 11:29:07 -0700

    Dynamic content & footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// src/Footer.js
+
+import React from 'react'
+import './Footer.css'
+
+function Footer () {
+    return (
+        <div className="Footer">
+            <h1>Muhammad Tifak copyright 2020</h1>
+        </div>
+    )
+}
+
+export default Footer
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,14 +1,15 @@
 // src/Project.js
 
 import React from 'react'
+import './POPOSSpace.css'
 
 function POPOSSpace(props) {
     const { name, image, address } = props
     return (
-      <div>
+      <div className="POPOSSpace">
         <h1>{name}</h1>
-        <div>{address}</div>
         <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+        <div>{address}</div>
       </div>
     )
   }
