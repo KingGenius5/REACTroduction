7fbf50f4566378ba128eea9fd167b2c497527f0b
Muhammad Tifak <moht20009@gmail.com>
2020-05-12 06:58:27 -0700

    Added times + deconstructing props

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -5,47 +5,27 @@ import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
 import data from './sfpopos-data.json'
 
+
 function POPOSList() {
+
+  const spaces = data.map((obj) => {
+    const { title, address, images, hours } = obj
+  
+    return (
+      <POPOSSpace
+        name={title}
+        address={address}
+        image={images[0]}
+        hours={hours}
+      />
+    )
+  })
+
   return (
     <div className="POPOSList">
-      <POPOSSpace
-        name="50 California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
-      />
-      <POPOSSpace
-        name="100 Pine"
-        address="100 Pine St."
-        image="100-pine.jpg" 
-      />
-      <POPOSSpace
-        name="101 California"
-        address="101 California St."
-        image="101-california.jpg" 
-      />
-      <POPOSSpace
-        name="Sansome Roof Garden"
-        address="343 Garden St."
-        image="343-sansome-roof-garden.jpg" 
-      />
-      <POPOSSpace
-        name="Market Street Plaza"
-        address="525 Market St."
-        image="525-market-street-plaza.jpg" 
-      />
-      <POPOSSpace
-        name="555 Cali"
-        address="555 California St."
-        image="555-california.jpg" 
-      />
+      { spaces }
     </div>
   )
 }
-
-const titles = data.map((obj) => {
-  return <h1>{obj.title}</h1>
-})
-
-console.log(titles)
 
 export default POPOSList
